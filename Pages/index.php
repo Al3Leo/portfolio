1402fe37de040fe6056e2ed1904a248cf84340aa
ale <a.leo88@studenti.unisa.it>
2025-12-17 01:23:44 +0100

    difinitivo per ora

---
+++ Pages/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Alessio Leo</title>
+    <meta name="author" content="Alessio Leo">
+</head>
+<body>
+
+<?php
+session_start();
+
+require_once 'sfondo_loop.html';
+require_once 'topbar.html';
+
+if (isset($_GET['lang'])) {
+    $_SESSION['lang'] = $_GET['lang'];
+} else
+    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '', 0, 2);
+
+if (isset($_SESSION['lang'])) {
+    $lang = $_SESSION['lang'];
+} else {
+    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '', 0, 2);
+}
+
+if ($lang === 'it') {
+    require_once 'info_ita.html';
+} else {
+    require_once 'info_eng.html';
+}
+?>
+
+</body>
+</html>
